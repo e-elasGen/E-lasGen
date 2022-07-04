4a03c13d52b7ae8dadb90b18ba69783d7e9dffdc
Vanessa Basilio <106209900+VanessaB063013@users.noreply.github.com>
2022-07-04 08:44:34 -0300

    Projeto integrador e-lasGen.

---
+++ elasgen/src/main/java/br/com/elasgen/elasgen/ELasgenApplication.java
@@ -0,0 +1,13 @@
+package br.com.elasgen.elasgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELasgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELasgenApplication.class, args);
+	}
+
+}
