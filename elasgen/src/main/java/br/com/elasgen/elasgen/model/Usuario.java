cc81bef70f54de6cdc9208370bc526129e3b3b8e
Dario Reis <guttyerresjunior1995@gmail.com>
2022-07-13 16:56:06 -0300

    adiciona task de segurança, testada e aprovada

--- elasgen/src/main/java/br/com/elasgen/elasgen/model/Usuario.java
+++ elasgen/src/main/java/br/com/elasgen/elasgen/model/Usuario.java
@@ -24,15 +24,19 @@ public class Usuario {
 	private long id;
 	
 	@NotNull
+	@Size(min=2, max=50)
 	private String nome;
 	
 	@NotNull
 	@Email
+	@Size(min=2,max=50)
 	private String usuario;
 	
 	@NotNull
+	@Size(min=8,max=12, message=" Mínimo de 8 caracteres e no máximo 12 caracteres ")
 	private String senha;
 	
+	@Size(min=1,max=1000)
 	private String foto;
 	
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
