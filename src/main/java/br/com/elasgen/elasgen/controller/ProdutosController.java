29b7c4fe88f9b585fb82322408af06c4ee28017c
Vanessa Basilio Jardim <vanessajardim063013@gmail.com>
2022-07-18 10:26:39 -0300

    Deploy do Projeto Integrador

---
+++ src/main/java/br/com/elasgen/elasgen/controller/ProdutosController.java
@@ -0,0 +1,66 @@
+package br.com.elasgen.elasgen.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.elasgen.elasgen.model.Produtos;
+import br.com.elasgen.elasgen.repository.ProdutosRepository;
+
+@RestController
+@RequestMapping("produtos")
+@CrossOrigin( origins = "*" , allowedHeaders = "*")
+public class ProdutosController {
+	
+	
+	@Autowired
+	private ProdutosRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produtos>> getall(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produtos> getById(@PathVariable long id){
+		return repository.findById(id).map(resp-> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());	
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<Produtos> post (@RequestBody Produtos produtos){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produtos));
+		
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produtos> put (@RequestBody Produtos produtos){
+		return ResponseEntity.ok(repository.save(produtos));
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Produtos>> getByDescricao(@PathVariable String descricao ){
+		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
+		}
+	
+	
+
+
+}
---
+++ src/test/java/br/com/elasgen/elasgen/ElasgenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elasgen.elasgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
